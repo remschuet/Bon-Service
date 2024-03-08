86a9af90c09ba0d9943171f723ceacc4463387bc
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-07 21:09:34 -0500

    fix: fixed UserTypes enum in prisma schema

---
+++ dev/kitchencompanion/prisma/migrations/20240308020040_user_type_fix/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "userType" "UserTypes" NOT NULL DEFAULT 'ADMIN';
--- dev/kitchencompanion/src/server/db/dao_user.ts
+++ dev/kitchencompanion/src/server/db/dao_user.ts
@@ -1,31 +1,28 @@
-import { PrismaClient } from "@prisma/client";
-import { User } from "../type";
+import { PrismaClient, UserTypes, User } from "@prisma/client";
 import * as argon2 from "argon2";
-
 
 const db = new PrismaClient();
 
-export async function createUser(user:User){
+export async function createUser(user: User) {
+  const hashedPassword = await argon2.hash(user.password);
 
-    const hashedPassword = await argon2.hash(user.password);
-
-    return await db.user.create({
-        data: {
-            email: user.email,
-            password: hashedPassword
-        }
-    });
+  return await db.user.create({
+    data: {
+      email: user.email,
+      password: hashedPassword,
+    },
+  });
 }
 
-export async function getUsers(){
-    return await db.user.findMany();
-} 
+export async function getUsers() {
+  return await db.user.findMany();
+}
 
-export async function updateUserRole(email:string){
-    return await db.user.update({
-        where: { email },
-        data: { 
-            userTypes: ["ADMIN"] 
-        }
-    });
+export async function updateUserRole(email: string) {
+  return await db.user.update({
+    where: { email },
+    data: {
+      userType: UserTypes.ADMIN,
+    },
+  });
 }
