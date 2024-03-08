431844cb0bdcd64e7ef7437fb21ac2c70d3a07d7
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-08 13:03:46 -0500

    feat: registration page

--- dev/kitchencompanion/src/app/page.tsx
+++ dev/kitchencompanion/src/app/page.tsx
@@ -1,13 +1,9 @@
-import { CreateUser } from "@/components/basic_test/create_users";
-import { DisplayUsers } from "@/components/basic_test/display_users";
+import { RegisterForm } from "@/components/register/register_form";
 
 export default function Home() {
-  return (
-    <main className="grid place-content-center">
-      <h1 className="font-bold text-center">Welcome to KitchenCompanion</h1>
-      <p>A recipe and cost management application for the modern chef</p>
-      <CreateUser />
-      <DisplayUsers />
-    </main>
-  );
+    return (
+        <main className="grid place-content-center">
+            <RegisterForm />
+        </main>
+    );
 }
