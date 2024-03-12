1c47acf3395c81e6504d773eb2a92c1e0c6a7efc
Remi Chuet <you@example.com~>
2024-03-12 12:15:04 -0400

    db: added create, get, to kitchen

---
+++ dev/kitchencompanion/src/data_access/kitchen.ts
@@ -0,0 +1,28 @@
+import { User, Kitchen } from "@prisma/client";
+import { db } from "@/db/prisma_db";
+
+export async function createKitchen(kitchen: Kitchen) {
+  return await db.kitchen.create({
+    data: {
+      userId: kitchen.userId,
+      name: kitchen.name 
+    },
+  });
+}
+
+export async function getAllKitchen(user: User) {
+    return await db.kitchen.findMany({
+      where: { userId: user.id },
+    });
+}
+
+export async function deleteKitchen(kitchen: Kitchen) {
+    return await db.kitchen.delete({
+      where: { id: kitchen.id },
+    });
+}
+
+// DEV FONCTIONS
+export async function dev_getAllKitchen() {
+    return await db.kitchen.findMany();
+}
