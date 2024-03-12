e0c9ef6eaeae351ff945f80c5974cb5b0cebc29a
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-12 13:41:41 -0400

    feat: augmented JWT token and sessions

--- dev/kitchencompanion/src/data_access/user.ts
+++ dev/kitchencompanion/src/data_access/user.ts
@@ -2,45 +2,51 @@ import { UserTypes, User } from "@prisma/client";
 import { db } from "@/db/prisma_db";
 
 export async function createUser(user: User) {
-  if (user.userType === UserTypes.MEMBER) {
-    return await db.user.create({
-      data: {
-        email: user.email,
-        password: user.password,
-        userType: UserTypes.MEMBER,
-      },
-    });
-  }
+    if (user.userType === UserTypes.MEMBER) {
+        return await db.user.create({
+            data: {
+                email: user.email,
+                password: user.password,
+                userType: UserTypes.MEMBER,
+            },
+        });
+    }
 
-  return await db.user.create({
-    data: {
-      email: user.email,
-      password: user.password,
-    },
-  });
+    return await db.user.create({
+        data: {
+            email: user.email,
+            password: user.password,
+        },
+    });
 }
 
 export async function getUsers() {
-  return await db.user.findMany();
+    return await db.user.findMany();
 }
 
 export async function updateUserRole(email: string) {
-  return await db.user.update({
-    where: { email },
-    data: {
-      userType: UserTypes.ADMIN,
-    },
-  });
+    return await db.user.update({
+        where: { email },
+        data: {
+            userType: UserTypes.ADMIN,
+        },
+    });
 }
 
 export async function deleteUser(email: string) {
-  return await db.user.delete({
-    where: { email },
-  });
+    return await db.user.delete({
+        where: { email },
+    });
 }
 
 export async function getUser(email: string) {
-  return await db.user.findUnique({
-    where: { email },
-  });
+    return await db.user.findUnique({
+        where: { email },
+    });
+}
+
+export async function getUserById(id: string) {
+    return await db.user.findUnique({
+        where: { id },
+    });
 }
