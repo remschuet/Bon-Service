8250585d1d47a144d25d5f8ef309b8380ebbdff1
Remi Chuet <you@example.com~>
2024-03-12 11:07:33 -0400

    feat: added ui to create supplier

---
+++ dev/kitchencompanion/src/components/basic_test/create_supplier.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createSupplier } from "@/data_access/supplier";
+import { revalidatePath } from "next/cache";
+import { Supplier } from "@prisma/client";
+
+export function CreateSupplier() {
+
+    async function handleCreateSupplier(formData: FormData){
+        'use server'
+        const newSupplier = {
+            name: formData.get("name") as string,
+        }
+
+        await createSupplier(newSupplier as Supplier);
+        revalidatePath("/test");
+    }
+
+    return(
+        <Card className="w-[350px] h-[450px] grid place-content-center">
+            <CardHeader>Create User</CardHeader>
+            <CardContent>
+                <form action={handleCreateSupplier} className="grid gap-2">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Enter name" />
+                    <Button type="submit">Create Supplier</Button>
+                </form>
+            </CardContent>
+        </Card>
+    );
+}
