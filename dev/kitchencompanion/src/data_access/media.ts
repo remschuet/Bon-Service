a0f1c85714e3ae47bab6b5b13bf420fc2ae3c511
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-12 09:58:03 -0400

    chore: added demo route terms

--- dev/kitchencompanion/src/data_access/media.ts
+++ dev/kitchencompanion/src/data_access/media.ts
@@ -1,8 +1,9 @@
 import {
-  DeleteObjectCommand,
-  GetObjectCommand,
-  PutObjectCommand,
+    DeleteObjectCommand,
+    GetObjectCommand,
+    PutObjectCommand,
 } from "@aws-sdk/client-s3";
+
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { aws } from "@/db/aws_db";
 
@@ -11,34 +12,34 @@ import "dotenv/config";
 const bucketName = process.env.BUCKET_NAME;
 
 export async function uploadImageToS3(image: Buffer, key: string) {
-  const params = {
-    Bucket: bucketName,
-    Key: key,
-    Body: image,
-    ContentType: "image/jpeg",
-  };
-  const command = new PutObjectCommand(params);
-  return aws.send(command);
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: image,
+        ContentType: "image/jpeg",
+    };
+    const command = new PutObjectCommand(params);
+    return aws.send(command);
 }
 
 export async function deleteImageFromS3(key: string) {
-  const params = {
-    Bucket: bucketName,
-    Key: key,
-  };
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+    };
 
-  const command = new DeleteObjectCommand(params);
-  return aws.send(command);
+    const command = new DeleteObjectCommand(params);
+    return aws.send(command);
 }
 
 // // GET FUNCTIONS:
 
 export async function getImageFromS3(key: string) {
-  return await getSignedUrl(
-    aws,
-    new GetObjectCommand({
-      Bucket: bucketName,
-      Key: key,
-    })
-  );
+    return await getSignedUrl(
+        aws,
+        new GetObjectCommand({
+            Bucket: bucketName,
+            Key: key,
+        })
+    );
 }
