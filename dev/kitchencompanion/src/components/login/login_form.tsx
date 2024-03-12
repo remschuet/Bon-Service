9d5c21dddb168d863c9144d956eb950e2c3f3f29
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-12 12:00:21 -0400

    feat: signIn functionality added

--- dev/kitchencompanion/src/components/login/login_form.tsx
+++ dev/kitchencompanion/src/components/login/login_form.tsx
@@ -11,126 +11,131 @@ import { Button } from "@/components/ui/button";
 import { CardContent, CardHeader } from "@/components/ui/card";
 
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
 import { FormError } from "@/components/form-error";
 
 import { useState } from "react";
+
 import { login } from "@/actions/login";
 
 export function LoginForm() {
-  const [error, setError] = useState("");
+    const [error, setError] = useState("");
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-    setError("");
-
-    login(values).then((res) => {
-      if (res.error) {
-        setError(res.error);
-        return;
-      }
-      console.log(res.success);
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
     });
-  };
 
-  return (
-    <div className='flex flex-col justify-center min-w-[500px]'>
-      <CardHeader className='flex flex-col space-y-2 text-center'>
-        <h1 className='text-2xl font-semibold tracking-tight'>Connexion</h1>
-        <p className='text-sm text-muted-foreground'>
-          Entrez votre adresse courriel et votre mot de passe
-        </p>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form
-            className='grid gap-2'
-            onSubmit={form.handleSubmit(onSubmit)}>
-            <div>
-              <FormField
-                control={form.control}
-                name='email'
-                render={({ field }) => (
-                  <FormItem className='relative'>
-                    <FormLabel
-                      className='sr-only'
-                      htmlFor='email'>
-                      Courriel
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder='nom@example.com'
-                        type='email'
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+    const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
+        setError("");
 
-              <FormField
-                control={form.control}
-                name='password'
-                render={({ field }) => (
-                  <FormItem className='relative'>
-                    <FormLabel
-                      className='sr-only'
-                      htmlFor='password'>
-                      Mot de passe
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder='Mot de passe'
-                        type='password'
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
+        login(values).then((res) => {
+            if (res && res.error) {
+                setError(res.error);
+                return;
+            }
+        });
+    };
 
-            {error !== "" && <FormError error={error} />}
+    return (
+        <div className="flex flex-col justify-center min-w-[500px]">
+            <CardHeader className="flex flex-col space-y-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Connexion
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                    Entrez votre adresse courriel et votre mot de passe
+                </p>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form
+                        className="grid gap-2"
+                        onSubmit={form.handleSubmit(onSubmit)}
+                    >
+                        <div>
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem className="relative">
+                                        <FormLabel
+                                            className="sr-only"
+                                            htmlFor="email"
+                                        >
+                                            Courriel
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="nom@example.com"
+                                                type="email"
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
 
-            <Button
-              variant={"default"}
-              type='submit'>
-              Connexion
-            </Button>
-            <div>
-              <p className='text-[0.75rem] text-center font-normal text-muted-foreground'>
-                Vous n'avez pas de compte?{" "}
-                <Link
-                  className='underline italic font-semibold'
-                  href='/register'>
-                  Inscrivez-vous!
-                </Link>
-              </p>
-              <p className='px-5 text-[0.7rem] text-muted-foreground text-center'>
-                <Link
-                  className='underline italic font-semibold'
-                  href='/forgot-password'>
-                  Mot de passe oublié?
-                </Link>
-              </p>
-            </div>
-          </form>
-        </Form>
-      </CardContent>
-    </div>
-  );
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem className="relative">
+                                        <FormLabel
+                                            className="sr-only"
+                                            htmlFor="password"
+                                        >
+                                            Mot de passe
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                placeholder="Mot de passe"
+                                                type="password"
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        {error !== "" && <FormError error={error} />}
+
+                        <Button variant={"default"} type="submit">
+                            Connexion
+                        </Button>
+                        <div>
+                            <p className="text-[0.75rem] text-center font-normal text-muted-foreground">
+                                Vous n'avez pas de compte?{" "}
+                                <Link
+                                    className="underline italic font-semibold"
+                                    href="/register"
+                                >
+                                    Inscrivez-vous!
+                                </Link>
+                            </p>
+                            <p className="px-5 text-[0.7rem] text-muted-foreground text-center">
+                                <Link
+                                    className="underline italic font-semibold"
+                                    href="/forgot-password"
+                                >
+                                    Mot de passe oublié?
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
+                </Form>
+            </CardContent>
+        </div>
+    );
 }
--- dev/kitchencompanion/src/db/prisma_db.ts
+++ dev/kitchencompanion/src/db/prisma_db.ts
@@ -1,19 +1,18 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
 
 declare global {
-  var prisma: PrismaClient | undefined;
-  var aws: S3Client | undefined;
+    var prisma: PrismaClient | undefined;
+    var aws: S3Client | undefined;
 }
 
 export const db =
-  globalThis.prisma ||
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+    globalThis.prisma ||
+    new PrismaClient({
+        log:
+            process.env.NODE_ENV === "development"
+                ? ["query", "error", "warn"]
+                : ["error"],
+    });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
