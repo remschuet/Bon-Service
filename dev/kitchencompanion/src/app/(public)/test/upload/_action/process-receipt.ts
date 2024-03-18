140ca3a9216d58519ccbddc8d6957048f5dab178
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-03-17 23:19:35 -0400

    style: added input and changed style of the upload test component

--- dev/kitchencompanion/src/app/(public)/test/upload/_action/process-receipt.ts
+++ dev/kitchencompanion/src/app/(public)/test/upload/_action/process-receipt.ts
@@ -1,50 +1,16 @@
 "use server";
 
-import { getReceiptData } from "@/lib/receipt-extraction/gpt-4-conversion";
-// import { convertPdfToPng } from "@/lib/receipt-extraction/pdf-to-png";
-import { convertReceiptToText } from "@/lib/receipt-extraction/receipt-extraction";
-
 export default async function processReceipt(formData: FormData) {
-  const file = formData.get("file") as File;
+  const headers = new Headers();
+  headers.append("X-API-KEY", process.env.RECEIPT_EXTRACTOR_API_KEY as string);
+  headers.append("X-Supplier", formData.get("supplier") as string);
 
-  if (!file) {
-    return { error: "Aucun fichier n'a été envoyé.", status: 400 };
-  }
+  const data = await fetch("http://127.0.0.1:5000/api/process-receipts", {
+    method: "POST",
+    headers: headers,
+    body: formData,
+  });
 
-  let buffer: Buffer;
-
-  if (file.type === "image/jpeg" || file.type === "image/png") {
-    const image = await file.arrayBuffer();
-    buffer = Buffer.from(image);
-  } //else if (file.type === "application/pdf") {
-  //   // const pdfData = await file.arrayBuffer();
-  //   // buffer = Buffer.from(pdfData);
-  //   // const pdf = await convertPdfToPng(buffer);
-  //   // if (pdf.length > 1) {
-  //   //   return {
-  //   //     error: "Le PDF ne doit contenir qu'une seule page.",
-  //   //     status: 400,
-  //   //   };
-  //   // }
-  //   // buffer = pdf[0].content;
-  else {
-    return {
-      error:
-        "Le type de fichier n'est pas supporté. Seulement les images et les PDF sont supportés.",
-      status: 400,
-    };
-  }
-
-  const data = await convertReceiptToText(buffer);
-
-  console.log(data);
-
-  const gptData = await getReceiptData(data, "birri");
-
-  const json = gptData.choices[0].message
-    .content!.replace("```json\n", "")
-    .replace("\n", "")
-    .replace("```", "");
-
-  return JSON.parse(json);
+  const response = await data.json();
+  console.log(response);
 }
--- dev/kitchencompanion/src/app/(public)/test/upload/page.tsx
+++ dev/kitchencompanion/src/app/(public)/test/upload/page.tsx
@@ -1,29 +1,18 @@
 import { Button } from "@/components/ui/button";
+import processReceipt from "@/app/(public)/test/upload/_action/process-receipt";
+import { Input } from "@/components/ui/input";
 
 export default async function Upload() {
-  const headers = new Headers();
-  headers.append("X-API-KEY", process.env.RECEIPT_EXTRACTOR_API_KEY as string);
-  headers.append("X-Supplier", "hector_larivee");
-
-  async function handleReceipt(formData: FormData) {
-    "use server";
-
-    const data = await fetch("http://127.0.0.1:5000/api/process-receipts", {
-      method: "POST",
-      headers: headers,
-      body: formData,
-    });
-
-    const response = await data.json();
-    console.log(response);
-  }
-
   return (
-    <form action={handleReceipt}>
-      <input
+    <form action={processReceipt}>
+      <Input
         type='file'
         name='file'
         accept='image/*, application/pdf'
+      />
+      <Input
+        type='text'
+        name='supplier'
       />
       <Button type='submit'>Upload</Button>
     </form>
