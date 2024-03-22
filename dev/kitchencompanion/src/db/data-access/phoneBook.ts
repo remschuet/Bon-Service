e681c2e67e70decd4c3155ae7de314e3494e6a05
Remi Chuet <remschuet@gmail.com>
2024-03-22 15:20:45 -0400

    db: create data access for: phoneBook.ts

---
+++ dev/kitchencompanion/src/db/data-access/phoneBook.ts
@@ -0,0 +1,5 @@
+import { RecipeBook, Allergen, Recipe } from "@prisma/client";
+import { db } from "@/db/prisma-db";
+import { Prisma } from "@prisma/client";
+
+export async function createPhoneBook(phoneBook: PhoneBook) {}
