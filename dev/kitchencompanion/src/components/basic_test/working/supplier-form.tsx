15bb857ff3c0f0879eb5d0908307c346d0e9279d
Remi Chuet <remschuet@gmail.com>
2024-03-24 13:05:44 -0400

    docs: added strucrure basic_test/working

---
+++ dev/kitchencompanion/src/components/basic_test/working/supplier-form.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Ingredient } from "@prisma/client";
+import React, { useState } from "react";
+import { useTransition } from "react";
+import { CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  actionGetIngredients,
+  actiionGetSupplierNameById,
+} from "../../../app/(public)/test/supplier/_action/supplier-action";
+
+export function SupplierForm() {
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [isPending, startTransition] = useTransition();
+
+  async function buildIngredientList(formData: FormData) {
+    startTransition(async () => {
+      const supplierId = formData.get("supplierName") as string;
+      const supplierName = await actiionGetSupplierNameById(supplierId);
+      if (supplierName) {
+        actionGetIngredients(supplierName).then((ingredients) => {
+          setIngredients(ingredients);
+        });
+      } else {
+        console.error("Supplier name is undefined.");
+      }
+    });
+  }
+  return (
+    <div className="w-full">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Quantity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {ingredients.map((row) => (
+            <TableRow key={row.id} className="flex flex-row">
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.unitPrice}</TableCell>
+              <TableCell>{row.unitMeasure}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <CardContent>
+        <form action={buildIngredientList} className="grid gap-2">
+          <input
+            type="text"
+            name="supplierName"
+            id="supplierName"
+            placeholder="Enter supplier Name"
+          />
+          <Button type="submit">
+            display ingredients for supplier Name(DEV)
+          </Button>
+        </form>
+      </CardContent>
+    </div>
+  );
+}
