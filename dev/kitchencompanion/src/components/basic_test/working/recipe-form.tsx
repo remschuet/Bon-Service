2719a2756b8f9be2834c361787f93a0fabfc8e79
Remi Chuet <remschuet@gmail.com>
2024-03-28 08:56:42 -0400

    feat: change supplier form for recipe form

---
+++ dev/kitchencompanion/src/components/basic_test/working/recipe-form.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import {} from "@/app/[locale]/(public)/test/recipe/_action/recipe-action";
+
+export function RecipeForm() {
+  return <div className="w-full">RecipeBuilder</div>;
+}
