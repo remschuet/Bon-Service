5215380e64841451f49c8b60b27136766ad0464e
Remi Chuet <remschuet@gmail.com>
2024-04-11 19:38:33 -0400

    feat: adding form and action in settings

---
+++ dev/kitchencompanion/src/app/[locale]/(protected)/settings/_components/test.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Ingredient, UnitMeasure } from "@prisma/client";
+import {
+  updateUserFacturation,
+  updateToAdmin,
+  updateProfilUser,
+} from "../_action/settings-action";
+import { useSession } from "@/hooks/useSession";
+
+export function Test() {
+  const { id } = useSession();
+
+  async function updateFacturation(formData: FormData) {
+    formData.append("userId", id);
+    console.log(await updateUserFacturation(formData));
+  }
+
+  async function updateAdminMode() {
+    console.log(await updateToAdmin(id));
+  }
+
+  async function updateProfil(formData: FormData) {
+    formData.append("userId", id);
+    console.log(await updateProfilUser(formData));
+  }
+
+  return (
+    <>
+      <form action={updateFacturation}>
+        <div>
+          <p>Bon Service Pro</p>
+          <input type="checkbox" name="facturation" value="PREMIUM" />
+        </div>
+        <Button type="submit">Modifier la facturation</Button>
+      </form>
+
+      <p>Update To admin Mode</p>
+      <form action={updateAdminMode}>
+        <Button type="submit">Admin Mode</Button>
+      </form>
+    </>
+  );
+}
--- dev/kitchencompanion/src/app/[locale]/(protected)/settings/page.tsx
+++ dev/kitchencompanion/src/app/[locale]/(protected)/settings/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { useRedirectMembers } from "@/hooks/useRedirectMembers";
+import { Test } from "./_components/test";
 
 export default function SettingsPage() {
   useRedirectMembers();
 
-  return <div>Parametres de compte</div>;
+  return (
+    <>
+      <h1>Parametres de compte (Le UI de Remi)</h1>
+      <Test />
+    </>
+  );
 }
