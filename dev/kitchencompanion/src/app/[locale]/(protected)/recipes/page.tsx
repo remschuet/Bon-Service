1239ff775bf3662b0993c957c2b84f18f201421f
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-04-12 14:44:19 -0400

    feat (frontend): started work on recipe interface.

--- dev/kitchencompanion/src/app/[locale]/(protected)/recipes/page.tsx
+++ dev/kitchencompanion/src/app/[locale]/(protected)/recipes/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useRedirectMembers } from "@/hooks/useRedirectMembers";
+import { RecipeGeneral } from "@/app/[locale]/(protected)/recipes/_components/recipe-general";
+import { RecipeInputBox } from "@/app/[locale]/(protected)/recipes/_components/recipe-input-box";
 
 export default function RecipePage() {
   useRedirectMembers();
 
-  return <div></div>;
+  return (
+    <div>
+      <RecipeGeneral />
+      <RecipeInputBox />
+    </div>
+  );
 }
