4ceaaed2f569a78222895bc645b645d2ba8b7632
Remi Chuet <you@example.com~>
2024-04-18 12:22:54 -0400

    feat: default value when created new user

--- dev/kitchencompanion/src/app/[locale]/(public)/account-verification/_actions/account-verification.ts
+++ dev/kitchencompanion/src/app/[locale]/(public)/account-verification/_actions/account-verification.ts
@@ -5,6 +5,7 @@ import {
   deleteVerificationToken,
 } from "@/db/data-access/verification-token";
 import { getUser, userVerification } from "@/db/data-access/user";
+import { account_init } from "@/app/[locale]/(public)/account-verification/_actions/account-init";
 
 export async function accountVerification(token: string) {
   const currentToken = await getVerificationTokenByToken(token);
@@ -25,6 +26,9 @@ export async function accountVerification(token: string) {
 
   await userVerification(user, currentToken);
   await deleteVerificationToken(currentToken.token);
+
+  // TODO: enlever le console.log gerer le retour  
+  console.log(await account_init(user.id));
 
   return { success: "Votre courriel a été vérifié avec succès.", status: 200 };
 }
