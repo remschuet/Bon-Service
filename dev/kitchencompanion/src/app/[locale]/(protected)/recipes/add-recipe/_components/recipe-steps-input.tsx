e7feb2185e22ca6e42e293c323ac3ae46a495e66
Julien Coulombe-Morency <jcoulombemorency@gmail.com>
2024-04-19 18:30:28 -0400

    feat: Added functionnality for managing recipe steps and updating the context with the proper values. Editing and deleting is also implemented in this commit.

--- dev/kitchencompanion/src/app/[locale]/(protected)/recipes/add-recipe/_components/recipe-steps-input.tsx
+++ dev/kitchencompanion/src/app/[locale]/(protected)/recipes/add-recipe/_components/recipe-steps-input.tsx
@@ -10,43 +10,73 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Badge } from "@/components/ui/badge";
+import RecipeStepsList from "@/app/[locale]/(protected)/recipes/add-recipe/_components/steps/recipe-steps-list";
+
+import { useNewRecipe } from "@/hooks/useNewRecipe";
+import { useRef } from "react";
+import { Steps } from "@/contexts/new-recipe";
 
 export function RecipeStepsInput() {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const { ctx } = useNewRecipe();
+
+  const handleAddStep = () => {
+    if (textareaRef.current?.value) {
+      const newStep: Steps = {
+        step: textareaRef.current.value,
+        id: crypto.randomUUID(),
+      };
+      ctx.setSteps([...ctx.steps, newStep]);
+      textareaRef.current.value = "";
+    }
+  };
+
   return (
-    <div className="relative flex min-h-[50vh] flex-col rounded-xl bg-stone-200 p-4 lg:col-span-2 min-w-[45vw]">
-      <Badge variant={"secondary"} className="absolute right-3 top-3">
+    <div className='relative flex min-h-[50vh] flex-col rounded-xl bg-stone-200 p-4 lg:col-span-2 max-w-[45vw]'>
+      <Badge
+        variant={"secondary"}
+        className='absolute right-3 top-3'>
         Étapes
       </Badge>
-      <div className="flex-1" />
-      <form
-        className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
-        x-chunk="dashboard-03-chunk-1"
-      >
-        <Label htmlFor="message" className="sr-only">
+      <div className='flex-1 mt-8'>
+        <RecipeStepsList />
+      </div>
+      <div
+        className='relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring'
+        x-chunk='dashboard-03-chunk-1'>
+        <Label
+          htmlFor='message'
+          className='sr-only'>
           Message
         </Label>
         <Textarea
-          id="message"
-          placeholder="Entrez votre étape ici..."
-          className="min-h-12 resize-none border-0 p-6 shadow-none focus-visible:ring-0"
+          ref={textareaRef}
+          placeholder='Entrez votre étape ici...'
+          className='min-h-12 resize-none border-0 p-6 shadow-none focus-visible:ring-0'
         />
-        <div className="flex items-center p-3 pt-0">
+        <div className='flex items-center p-3 pt-0'>
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Mic className="size-4" />
-                  <span className="sr-only">Utiliser le microphone</span>
+                <Button
+                  variant='ghost'
+                  size='icon'>
+                  <Mic className='size-4' />
+                  <span className='sr-only'>Utiliser le microphone</span>
                 </Button>
               </TooltipTrigger>
-              <TooltipContent side="top">Utiliser le microphone</TooltipContent>
+              <TooltipContent side='top'>Utiliser le microphone</TooltipContent>
             </Tooltip>
           </TooltipProvider>
-          <Button type="submit" size="sm" className="ml-auto gap-1.5">
+          <Button
+            size='sm'
+            className='ml-auto gap-1.5'
+            onClick={handleAddStep}>
             Ajouter
           </Button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
---
+++ dev/kitchencompanion/src/app/[locale]/(protected)/recipes/add-recipe/_components/steps/recipe-steps-item.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+import { BadgeCheck, BadgeX, Settings } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { useNewRecipe } from "@/hooks/useNewRecipe";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function RecipeStepsItem({
+  direction,
+  index,
+}: {
+  direction: string;
+  index: number;
+}) {
+  const { ctx } = useNewRecipe();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(direction);
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditedText(e.target.value);
+  };
+
+  const toggleEditing = () => {
+    if (isEditing) {
+      const updatedSteps = [...ctx.steps];
+      updatedSteps[index].step = editedText; // Update the specific step with the new edited text
+      ctx.setSteps(updatedSteps);
+    }
+    setIsEditing(!isEditing);
+  };
+
+  const handleRemoveDirection = () => {
+    const currentSteps = [...ctx.steps];
+    currentSteps.splice(index, 1);
+    ctx.setSteps(currentSteps);
+  };
+
+  return (
+    <div
+      className={`flex gap-2 w-full p-2 border rounded-lg  mb-2 ${
+        !isEditing ? "bg-stone-100 hover:bg-stone-300/50" : " bg-stone-300/50"
+      }`}>
+      <div className='flex gap-1 mr-5 my-auto'>
+        <div onClick={toggleEditing}>
+          {isEditing && (
+            <BadgeCheck
+              className='cursor-pointer text-emerald-500 hover:text-emerald-800'
+              size={20}
+            />
+          )}
+        </div>
+        <div onClick={handleRemoveDirection}>
+          {!isEditing && (
+            <BadgeX
+              className='cursor-pointer text-destructive/15 hover:text-red-700'
+              size={20}
+            />
+          )}
+        </div>
+      </div>
+      <Label className='flex gap-2 items-start w-full'>
+        <div className='text-sm font-semibold py-1 leading-5'>{index + 1}.</div>
+        {isEditing ? (
+          <Textarea
+            className='text-normal w-full bg-stone-100 ml-2'
+            value={editedText}
+            onChange={handleTextChange}
+          />
+        ) : (
+          <span
+            onClick={toggleEditing}
+            className='text-normal p-1 leading-5 cursor-pointer'>
+            {editedText}
+          </span>
+        )}
+      </Label>
+    </div>
+  );
+}
