edfc349fa03ec6628fd8c136a641ff1673951db6
Remi Chuet <you@example.com~>
2024-04-22 15:47:44 -0400

    feat: added action menu

--- dev/kitchencompanion/src/app/[locale]/(protected)/menus/_action/menus-action.ts
+++ dev/kitchencompanion/src/app/[locale]/(protected)/menus/_action/menus-action.ts
@@ -1,1 +1,14 @@
- 
+"use server";
+
+import {} from "@/db/data-access/menu";
+
+import { Menu } from "@prisma/client";
+
+/**
+ * Add menu to the user.
+ * 
+ * @param formData - The form data containing the ingredient details.
+ * @returns An object indicating the status of the operation.
+ */
+
+export async function addMenu(formData: FormData) {}
