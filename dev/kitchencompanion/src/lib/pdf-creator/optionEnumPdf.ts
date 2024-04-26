cc43d104c744f2a1d1f10de232b2379d18f6dbb4
Remi Chuet <remschuet@gmail.com>
2024-04-26 10:10:01 -0400

    feat: create pdf and table from jspdf

---
+++ dev/kitchencompanion/src/lib/pdf-creator/optionEnumPdf.ts
@@ -0,0 +1,18 @@
+// Utilisation d'enum pour OrientationPDF
+export enum OrientationPDF {
+  Portrait = "portrait",
+  Landscape = "landscape",
+}
+export enum UnitPDF {
+  mm = "mm",
+  cm = "cm",
+  in = "in",
+  px = "px",
+}
+
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export type ContactData = { [key: string]: string };
