86c121588f84ea6f5e3bb01fdb254a5aadf0a587
Remi Chuet <remschuet@gmail.com>
2024-05-04 16:25:04 -0400

    feat: added docs to search recipe

--- dev/kitchencompanion/src/app/[locale]/(protected)/settings/_export/pdf-recipe-export.tsx
+++ dev/kitchencompanion/src/app/[locale]/(protected)/settings/_export/pdf-recipe-export.tsx
@@ -1,4 +1,13 @@
+import { getRecipeIngredientAndRecipe } from "@/db/data-access/recipe";
+
 export async function exportCreatePdfRecipe(form: FormData) {
   const id = form.get("userId");
   const recipeName = form.get("recipeName");
+  const idRecipe = "1234";
+  const data = await getRecipeIngredientAndRecipe(idRecipe);
+  console.log(data);
+  return data;
+
+  // Chercher la recette + ingredients
+  // Chercher les noms des ingredients
 }
